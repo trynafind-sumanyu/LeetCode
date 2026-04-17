7d0ddd52bce3524c39d22e0cf0c70e46e504da0f
Sumanyu Rajput <162854999+trynafind-sumanyu@users.noreply.github.com>
2026-04-18 04:54:38 +0530

    Time: 142 ms (7.3%), Space: 52.5 MB (6.01%) - LeetHub

---
+++ 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    int[][][][][] dp = new int[10][2][2][1023][2];
+    public int numDupDigitsAtMostN(int n) {
+        String num = String.valueOf(n);
+        for(int[][][][] x : dp){
+            for(int[][][] y : x){
+                for(int[][] z : y){
+                    for(int[] a : z){
+                       Arrays.fill(a, -1);
+                    }
+                }
+            }
+        }
+        return solve(num,0,1,0,0,1,false);
+    }
+
+    private int solve(String num,int index, int tight, int rep,int mask,int lz, boolean isUsed){
+        if(index == num.length()){
+            return rep;
+        }
+
+        if(dp[index][tight][rep][mask][lz] != -1){
+            return dp[index][tight][rep][mask][lz];
+        }
+
+        int lb = 0;
+        int ub = (tight == 1) ? num.charAt(index) - '0' : 9;
+        int res = 0;
+        for(int dig = lb; dig <= ub; dig++){
+            int newTight = (tight == 1 && dig == ub) ? 1 : 0;
+            if(lz==1 && dig == 0){
+                res += solve(num,index+1,newTight,0,0,1,false);
+            }
+            else {
+                boolean isUsedNew = (1 & (mask >> dig)) == 1;
+                int newRep = (rep == 1 || isUsedNew) ? 1 : 0;
+                res += solve(num,index+1,newTight,newRep,(mask| 1 << dig),0,isUsedNew);
+            }
+        }
+        return dp[index][tight][rep][mask][lz] = res;
+    }
+
+
+}
