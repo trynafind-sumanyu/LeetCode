74b7b9860b800501d9c0d468074dbe1702f79889
Sumanyu Rajput <162854999+trynafind-sumanyu@users.noreply.github.com>
2026-05-01 06:38:58 +0530

    Time: 5 ms (21.8%), Space: 91.1 MB (8.27%) - LeetHub

---
+++ 0396-rotate-function/0396-rotate-function.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int maxRotateFunction(int[] nums) {
+        int n = nums.length;
+        int[] f = new int[n];
+        /*
+            f[0] = 0*[0] + 1*[1] + 2*[2] + 3*[3]
+            f[1] = 1*[0] + 2*[1] + 3*[2] + 0*[3]
+            f[2] = 2*[0] + 3*[1] + 0*[2] + 1*[3]
+            f[3] = 3*[0] + 0*[1] + 1*[2] + 2*[3]
+
+            f[1] - f[0] = 1[0] + 1[1] + 1[2] - 3[3]
+
+
+            f[2] - f[1] = 1[0] + 1[1] - 3[2] + 1[3] + 1[2] - 1[2] = 1[0]+1[1]+1[2]+1[3] - 4[2]
+            f[k] - f[k-1] =                                               Sum of Array  - n*[n-k]
+
+
+            f[k+1] = f[k] + Sum of Array - n*[n-k-1]  : k = 1 hence k+1 = 2 so 4-1-1 = 2 correct
+
+         */
+        int sum = 0;
+        for(int i : nums){
+            sum += i;
+        }
+
+        for(int i = 0;i<n;i++){
+            //f[0]
+            f[0] += nums[i]*i;
+        }
+
+        int max = f[0];
+        for(int i = 1;i<n;i++){
+            f[i] = f[i-1] + sum - n*nums[n-i];
+            max = Math.max(max,f[i]);
+        }
+        return max;
+    }
+}
