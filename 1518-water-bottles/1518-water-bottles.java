a46d33db8023a4916d5348173f96da8893e2d234
Sumanyu Rajput <162854999+trynafind-sumanyu@users.noreply.github.com>
2026-05-07 03:56:02 +0530

    Time: 0 ms (100%), Space: 42.2 MB (19.23%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.java
+++ 1518-water-bottles/1518-water-bottles.java
@@ -1,9 +1,10 @@
 class Solution {
-    public int numWaterBottles(int numBottles, int numExchange) {
-        int ans = numBottles;
-        while ((numBottles / numExchange) > 0) {
-            ans += (numBottles / numExchange);
-            numBottles = (numBottles % numExchange) + (numBottles / numExchange);
+    public int numWaterBottles(int b, int k) {
+        int ans = b;
+
+        while(b/k > 0){
+            ans += b/k;
+            b = b/k + b%k;
         }
         return ans;
     }
